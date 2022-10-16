68ea83b07a7900e03bd9985104e7cc6842f85bf8
henrynedu <henrynwachi77@gmail.com>
2022-10-16 13:40:24 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#iclude <stdio.h>
+/*
+*main - prints out a line of code using puts
+*
+*Return: 0
+*/
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
